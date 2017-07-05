7fea48dae00b8082657f58a27063be0af8dd652c
Aarick Lameman <lamemanaarick@gmail.com>
2017-07-05 09:55:23 -0600

    moved  folders, js, css, & fonts back into assets

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
 
-		<link rel="stylesheet" href="lib/css/main.css" />
+		<link rel="stylesheet" href="assets/css/main.css" />
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
 
-		<link rel="stylesheet" href="lib/css/style.css" />
+		<link rel="stylesheet" href="assetsassets/css/style.css" />
 
 		<!-- Latest compiled and minified CSS -->
 		<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
@@ -414,13 +414,13 @@
 		</div>
 
 		<!-- Scripts -->
-			<script src="lib/js/jquery.min.js"></script>
-			<script src="lib/js/jquery.dropotron.min.js"></script>
-			<script src="lib/js/skel.min.js"></script>
-			<script src="lib/js/skel-viewport.min.js"></script>
-			<script src="lib/js/util.js"></script>
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/skel-viewport.min.js"></script>
+			<script src="assets/js/util.js"></script>
 			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
-			<script src="lib/js/main.js"></script>
+			<script src="assets/js/main.js"></script>
 			<!-- Latest compiled and minified JavaScript -->
 			<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
 				integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
