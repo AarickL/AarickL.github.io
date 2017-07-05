5f3fe1b8dcbe0edfb0d791b66825ad4a63d64710
Aarick Lameman <lamemanaarick@gmail.com>
2017-07-05 09:46:52 -0600

    uncommented out the bootstrap code

--- index.php
+++ index.php
@@ -32,9 +32,9 @@
 		<link rel="stylesheet" href="lib/css/style.css" />
 
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+		<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
 			integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-			crossorigin="anonymous">
+			crossorigin="anonymous"> -->
 
 		<link rel="short icon" href="images/devLogo.ico"/>
 
@@ -422,9 +422,9 @@
 			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
 			<script src="lib/js/main.js"></script>
 			<!-- Latest compiled and minified JavaScript -->
-			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+			<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
 				integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-				crossorigin="anonymous"></script>
+				crossorigin="anonymous"></script> -->
 
 	</body>
 </html>
